8d2b51036960bff56ef39fc84f307817f901133b
NA <rushikeshgaikwad2565@gmail.com>
2026-03-04 17:52:08 +0530

    url added

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://gahuattabackend.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
